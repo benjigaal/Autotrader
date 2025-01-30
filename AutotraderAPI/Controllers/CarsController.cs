cc379f1e64ec89d1ed57e71f0965af11cef93e20
Gaál Gergely Benjámin <gaalgb@KKSZKI.LOCAL>
2025-01-30 18:56:39 +0100

    Post endpoint is ready to use.

---
+++ AutotraderAPI/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using AutotraderAPI.Models;
+using AutotraderAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutotraderAPI.Controllers
+{
+    [Route("cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult AddNewCar(CreateCarDto createdCarDto)
+        {
+            var car = new Car 
+            { 
+                Id = Guid.NewGuid(),
+                Brand = createdCarDto.Brand,
+                Type = createdCarDto.Type,
+                Color = createdCarDto.Color,
+                Myear = createdCarDto.Myear
+            };
+
+            using (var context = new AutotraderContext())
+            {
+                context.Cars.Add(car);
+                context.SaveChanges();
+
+                return StatusCode(201, new {result = car, message = "Sikeres felvétel."});
+            }
+            return Ok();
+        }
+    }
+}
