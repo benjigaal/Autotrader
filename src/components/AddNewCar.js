1282f8b2b6ae3637638dc9148dddd4d25c525dbd
Gaál Gergely Benjámin <GaalGB@KKSZKI.LOCAL>
2025-02-06 18:49:51 +0100

    Components are added.

---
+++ src/components/AddNewCar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddNewCar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddNewCar
---
+++ src/components/GetAllCars.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GetAllCars() {
+  return (
+    <div>
+      GetAllCars
+    </div>
+  )
+}
+
+export default GetAllCars
